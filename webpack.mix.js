6a32037b1e58dce7ed726215d0c02eb73bdc260a
Evgen <eok8177@gmail.com>
2018-02-12 18:09:22 +0200

    build collections of JS & CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,8 @@
 let mix = require('laravel-mix');
 
 mix
-	.js('js/main.js', 'builded/js')
-	.styles(
-		[
-			'css/main.css',
-			'css/main2.css',
-		],
-		'builded/css/main.css'
-	);
-    // .sass('sass/app.scss', 'builded/css');
+	.scripts('js/*.js', 'builded/js/main.js')
+	.styles('css/*.css','builded/css/main.css');
+
+	//.js('js/main.js', 'builded/main.js')
+	//.sass('sass/app.scss', 'builded/css');
